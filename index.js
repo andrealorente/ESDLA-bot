a9f06e8b743c957405f1e45a66233c517405c32f
Andrea <andrea_lorente_1994@hotmail.com>
2017-07-02 18:27:19 +0200

    citas json

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const Telegraf = require('telegraf');
+const fs = require ('fs');
 
 const app = new Telegraf("414752663:AAG0d1MDSQomeSaIpHYQjSK-D5J2bD9OhGk");
+var citas = {};
+
 app.command('start', ({ from, reply }) => {
   console.log('start', from);
-  return reply('Bienvenido al mejor Bot de citas de El Señor de los Anillos, '+ from.first_name +'!. Di "Llegas tarde" o manda un sticker');
+
+  fs.readFile("./citas.json", (err,data) =>{
+    if(err) throw err;
+    citas = JSON.parse(data);
+  });
+
+  return reply('Bienvenido al mejor Bot de citas de El Señor de los Anillos, '+ from.first_name +'!. Di "Llegas tarde" o manda un sticker. También puedes usar el comando /character');
 });
+
+function getRandomQuoteFrom(person){
+  for(var i = 0; i<citas.length; i++){
+
+    if(citas[i].author == person){
+      var random = Math.floor(Math.random() * citas[i].quotes.length);
+      console.log(citas[i].quotes[random]);
+      return citas[i].quotes[random];
+    }
+
+  }
+
+};
+
 app.command('/character', (ctx) => {
    // /character legolas
   var parts = ctx.message.text.split(" ");
@@ -18,19 +41,11 @@ app.command('/character', (ctx) => {
   if(character == "")
     ctx.reply('Escribe el nombre de algún personaje!!!');
   else{
-    if(character == "legolas")
-      ctx.reply('¡Se llevan a los hobbits a Isengard!');
-    else if(character == "aragorn")
-      ctx.reply('En este día lucharemos por todo aquello que nuestro corazón ama de esta buena tierra.');
-    else if(character == "frodo")
-      ctx.reply('Oh, Sam. <3');
-    else if(character == "gandalf")
-        ctx.reply('Con la primera luz del quinto día, al alba, mira al este.');
-    else if(character == "bilbo")
-      ctx.reply('No conozco a la mitad de vosotros la mitad de lo que desearía, y lo que deseo es menos de la mitad de lo que la mitad merecéis.');
+    var cita = getRandomQuoteFrom(character);
+    ctx.reply(cita);
   }
 });
 
-app.hears('Llegas tarde', (ctx) => ctx.reply('Un mago nunca llega tarde. Ni pronto, llega exactamente cuando se lo propone.'));
-app.on('sticker', (ctx) => ctx.reply('Mucho se perdió entonces pero nadie queda ahora para recordarlo.'));
+app.hears('Llegas tarde', (ctx) => ctx.replyWithMarkdown('*Un mago nunca llega tarde. Ni pronto, llega exactamente cuando se lo propone.*'));
+app.on('sticker', (ctx) => ctx.replyWithHTML('<i>Mucho se perdió entonces pero nadie queda ahora para recordarlo.</i>'));
 app.startPolling();
