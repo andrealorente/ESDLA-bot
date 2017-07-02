dbe928c908a841769a06833fcf5f339af443bd59
Andrea <andrea_lorente_1994@hotmail.com>
2017-07-02 13:47:58 +0200

    Prueba 2

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const Telegraf = require('telegraf');
 const app = new Telegraf("414752663:AAG0d1MDSQomeSaIpHYQjSK-D5J2bD9OhGk");
 app.command('start', ({ from, reply }) => {
   console.log('start', from);
-  return reply('Welcome!');
+  return reply('Bienvenido al mejor Bot de citas de El Señor de los Anillos, Jorge!. Di "Llegas tarde" o manda un sticker');
 });
-app.hears('hi', (ctx) => ctx.reply('Hey there!'));
-app.on('sticker', (ctx) => ctx.reply('👍'));
+app.hears('Llegas tarde', (ctx) => ctx.reply('Un mago nunca llega tarde. Ni pronto, llega exactamente cuando se lo propone.'));
+app.on('sticker', (ctx) => ctx.reply('Mucho se perdió entonces pero nadie queda ahora para recordarlo.'));
 app.startPolling();
