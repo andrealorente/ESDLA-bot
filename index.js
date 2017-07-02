cc40cfd2e0819de9c6c94b716dd876ba4ab2a1b8
Andrea <andrea_lorente_1994@hotmail.com>
2017-07-02 13:52:10 +0200

    x

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.command('start', ({ from, reply }) => {
   console.log('start', from);
   return reply('Bienvenido al mejor Bot de citas de El Señor de los Anillos, Jorge!. Di "Llegas tarde" o manda un sticker');
 });
+app.command('/character', (ctx) => ctx.reply('Legolas'));
 app.hears('Llegas tarde', (ctx) => ctx.reply('Un mago nunca llega tarde. Ni pronto, llega exactamente cuando se lo propone.'));
 app.on('sticker', (ctx) => ctx.reply('Mucho se perdió entonces pero nadie queda ahora para recordarlo.'));
 app.startPolling();
