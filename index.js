cde617b922368c21324cd7aabd665157617b08e7
Andrea <andrea_lorente_1994@hotmail.com>
2017-07-02 13:43:50 +0200

    Token string

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf');
 
-const app = new Telegraf(process.env.BOT_TOKEN);
+const app = new Telegraf("414752663:AAG0d1MDSQomeSaIpHYQjSK-D5J2bD9OhGk");
 app.command('start', ({ from, reply }) => {
   console.log('start', from);
   return reply('Welcome!');
