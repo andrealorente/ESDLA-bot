aa0118a8ead67a1d72531b52e639363fa34f9e58
Andrea <andrea_lorente_1994@hotmail.com>
2017-07-02 14:32:44 +0200

    Añadidos archivos necesarios de babel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Telegraf = require('telegraf');
 const app = new Telegraf("414752663:AAG0d1MDSQomeSaIpHYQjSK-D5J2bD9OhGk");
 app.command('start', ({ from, reply }) => {
   console.log('start', from);
-  return reply('Bienvenido al mejor Bot de citas de El Señor de los Anillos, Jorge!. Di "Llegas tarde" o manda un sticker');
+  return reply('Bienvenido al mejor Bot de citas de El Señor de los Anillos, '+ from.first_name +'!. Di "Llegas tarde" o manda un sticker');
 });
 app.command('/character', (ctx) => ctx.reply('Legolas'));
 app.hears('Llegas tarde', (ctx) => ctx.reply('Un mago nunca llega tarde. Ni pronto, llega exactamente cuando se lo propone.'));
